7323ea49b766832276af2b46bfcf57e626d6b915
sitarnet <sitarnet@ukr.net>
2022-04-25 15:43:02 +0300

    There was a test in your class and you passed it. Congratulations! But you're an ambitious person. You want to know if you're better than the average student in your class.
    You receive an array with your peers' test scores. Now calculate the average and compare your score!
    
    Return True if you're better, else False!
    
    Note:
    Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-def make_upper_case(s):
-    # Code here
-    s = s.upper()
-    return s
+# def make_upper_case(s):
+#     # Code here
+#     s = s.upper()
+#     return s
+#
+# #make_upper_case("eee")
+#
+# def string_to_array(s):
+#     b = s.split(" ")
+#     return b
+#
+# #a = string_to_array("I am Nikolay")
+# #print(a)
+#
+# def invert(lst):
+#     b = []
+#     for i in lst:
+#         if i > 0:
+#             b.append(-i)
+#         else:
+#             b.append(-i)
+#     return b
 
-#make_upper_case("eee")
+#c = invert([-1, 6, 5, -3, 4])
+#print(c)
 
-def string_to_array(s):
-    b = s.split(" ")
-    return b
 
-#a = string_to_array("I am Nikolay")
-#print(a)
+import statistics
 
-def invert(lst):
-    b = []
-    for i in lst:
-        if i > 0:
-            b.append(-i)
-        else:
-            b.append(-i)
-    return b
+def better_than_average(class_points, your_points):
+    middle = int(statistics.mean(class_points))
+    print(middle)
+    if middle < your_points:
+        return True
+    else:
+        return False
 
-c = invert([-1, 6, 5, -3, 4])
-print(c)
+b = better_than_average([3, 7], 2)
+print(b)
