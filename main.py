0f7897808eecf9f0031bddb77cda13ee34295bc3
sitarnet <sitarnet@ukr.net>
2022-04-25 13:00:48 +0300

    Write a function which converts string to list.

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ def make_upper_case(s):
     s = s.upper()
     return s
 
+#make_upper_case("eee")
 
-make_upper_case("eee")
+def string_to_array(s):
+    b = s.split(" ")
+    return b
+
+a = string_to_array("I am Nikolay")
+print(a)
