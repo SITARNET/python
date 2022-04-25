95250ac7a207a9b4ade8ab5248840c857563f928
sitarnet <sitarnet@ukr.net>
2022-04-25 15:20:15 +0300

    Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ def string_to_array(s):
     b = s.split(" ")
     return b
 
-a = string_to_array("I am Nikolay")
-print(a)
+#a = string_to_array("I am Nikolay")
+#print(a)
+
+def invert(lst):
+    b = []
+    for i in lst:
+        if i > 0:
+            b.append(-i)
+        else:
+            b.append(-i)
+    return b
+
+c = invert([-1, 6, 5, -3, 4])
+print(c)
