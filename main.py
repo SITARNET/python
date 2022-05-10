7451dfdf6c5460f7469fc2ee100c88a1d46b49f0
sitarnet <sitarnet@ukr.net>
2022-05-11 00:36:10 +0300

    Given an array of integers.
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
    
    If the input is an empty array or is null, return an empty array.
    
    Example
    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].

--- main.py
+++ main.py
@@ -692,12 +692,34 @@
 # print(is_valid_walk(['n','s','n','s','n','s','n','s','n','s']))
 
 
-def count_by(x, n):
-    # arr = []
-    # for i in range(1, n + 1):
-    #     i *= x
-    #     arr.append(i)
-    # return arr
-    return [i * x for i in range(1, n + 1)]
+# def count_by(x, n):
+#     # arr = []
+#     # for i in range(1, n + 1):
+#     #     i *= x
+#     #     arr.append(i)
+#     # return arr
+#     return [i * x for i in range(1, n + 1)]
+#
+# print(count_by(3, 10))
 
-print(count_by(3, 10))
+
+def count_positives_sum_negatives(arr):
+    # arr_new = []
+    # count_plus = 0
+    # a = 0
+    # if arr != []:
+    #     for i in arr:
+    #         if i > 0:
+    #             count_plus += 1
+    #         else:
+    #             a += i
+    #     arr_new.append(count_plus)
+    #     arr_new.append(a)
+    #     return arr_new
+    # else:
+    #     return arr_new
+
+    return [len([x for x in arr if x > 0])] + [sum(y for y in arr if y < 0)] if arr else []
+
+
+print(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
