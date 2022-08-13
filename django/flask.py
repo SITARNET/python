17b676b84f4b572006319801bffb37259b474fa6
sitarnet <sitarnet@ukr.net>
2022-08-13 17:41:13 +0300

    Flask 1. Что это такое? Простое WSGI-приложение.

---
+++ django/flask.py
@@ -0,0 +1,16 @@
+# 1. Flask
+
+# Flask - микрофреймворк для создания сайтов
+# WSGI - интерпретатор Python для запуска приложения на Flask
+# Apache -> WSGI -> Flask -> Представление (Представление N)
+# WSGI (Web Server Gateway Interface) - стандарт взаимодействия между Python-программой на стороне
+# сервера и самим веб-сервером.
+
+# pip install Flask
+# from flask import Flask
+
+# app = Flask(__name__) - экземпляр класса Flask
+
+# if __name__ == "__main__": - для запуска локального вервера
+#   app.run(debug=True) - режим откладки
+
