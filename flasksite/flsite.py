c3b0304108def962c166c0ba25647712d721895a
sitarnet <sitarnet@ukr.net>
2022-08-18 00:05:13 +0300

    Flask 5. Подключение внешних ресурсов и работа с формами.

--- flasksite/flsite.py
+++ flasksite/flsite.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 @app.route("/")
 def index():
@@ -14,18 +16,20 @@ def about():
     print(url_for('about'))
     return render_template('about.html', title='О сайте про Flask', menu=menu)
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form['username'])
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # with app.test_request_context():
 #     print(url_for("index"))
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username='sitarnet'))
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username='sitarnet'))
 
